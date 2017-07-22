83d065eb71bec7556ddc90823827cb16b3409f32
RENTdanplo <RENT@RENT-Komputer>
2017-07-22 11:05:15 +0200

    street starts

--- src/main/java/com/sda/Main.java
+++ src/main/java/com/sda/Main.java
@@ -2,6 +2,7 @@ package com.sda;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StringArrayDeserializer;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,6 +10,7 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
@@ -40,6 +42,28 @@ public class Main {
                     .forEach(System.out::println);
 
             //Sprawdź, czy jest osoba, której ulica zaczyna  sie na jakas litere
+
+            System.out.println("--------------------------");
+            personList
+                    .stream()
+                    .filter(p -> p.address.street.toUpperCase().startsWith("N"))
+                    .findAny().ifPresent(p -> System.out.println(p.name));
+
+            Optional<Person> person2 = personList
+                    .stream()
+                    .filter(p -> p.address.street.toUpperCase().startsWith("N"))
+                    .findAny();
+
+
+            System.out.println("-----------------------");
+            if (person2.isPresent()) {
+                System.out.println(person2.get().name);
+            }
+            else {
+                System.out.println("Nobody live in street starts with N");
+            }
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
