4b2386a0bdbf16b9dae9d368b35fc1b94fc9b9b2
RENTdanplo <RENT@RENT-Komputer>
2017-07-24 17:36:56 +0200

    dependency for Junit4 added

---
+++ src/test/java/com/sda/DummyTest.java
@@ -0,0 +1,16 @@
+package com.sda;
+
+import com.sun.xml.internal.ws.policy.AssertionSet;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by RENT on 2017-07-24.
+ */
+public class DummyTest {
+    @Test
+    public void shouldReturnTrue() throws Exception {
+        Assert.assertTrue(1==1);
+
+    }
+}
