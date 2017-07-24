f515d3c32ddbff411d071ee9e2bd5845ecd81fe2
RENTdanplo <RENT@RENT-Komputer>
2017-07-24 18:02:28 +0200

    client server init

---
+++ src/main/java/com/sda/client/Client.java
@@ -0,0 +1,11 @@
+package com.sda.client;
+
+/**
+ * Created by RENT on 2017-07-24.
+ */
+public class Client {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/main/java/com/sda/server/Server.java
@@ -0,0 +1,10 @@
+package com.sda.server;
+
+/**
+ * Created by RENT on 2017-07-24.
+ */
+public class Server {
+    public static void main(String[] args) {
+
+    }
+}
