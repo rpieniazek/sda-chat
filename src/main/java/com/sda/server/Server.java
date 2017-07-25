ad72e0e8fc54b98ecaf5e3368d2334df6139f22f
RENTdanplo <RENT@RENT-Komputer>
2017-07-25 20:17:18 +0200

    server side init

--- src/main/java/com/sda/server/Server.java
+++ src/main/java/com/sda/server/Server.java
@@ -1,10 +1,25 @@
 package com.sda.server;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by RENT on 2017-07-24.
  */
 public class Server {
     public static void main(String[] args) {
+        try {
+            ServerSocket listener  = new ServerSocket(8080);
+            Socket socket = listener.accept();
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
