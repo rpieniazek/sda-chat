ab4088e99cf9744a45e3f7533b1caad07177c791
RENTdanplo <RENT@RENT-Komputer>
2017-07-26 19:54:09 +0200

    ceasar encrypting, implemented encrypting method

--- src/main/java/com/sda/commons/Encrypter.java
+++ src/main/java/com/sda/commons/Encrypter.java
@@ -6,6 +6,15 @@ package com.sda.commons;
 public class Encrypter {
     public static String encrypt(String plainText) {
 
-        return plainText;
+        char[] text = plainText.toCharArray();
+        String result ="";
+        for(int i=0;i<text.length;i++){
+            char c = (char)(text[i]+3);
+            result +=c;
+        }
+
+        return result;
     }
+
+
 }
