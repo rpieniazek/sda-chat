3a9fe0443fdc7ab0fb6c7c19b68a74bfd3248ef6
RENTdanplo <RENT@RENT-Komputer>
2017-07-27 18:09:00 +0200

    messageDto added

---
+++ src/main/java/com/sda/commons/MessageDto.java
@@ -0,0 +1,36 @@
+package com.sda.commons;
+
+import java.time.LocalTime;
+
+/**
+ * Created by RENT on 2017-07-27.
+ */
+public class MessageDto {
+    private String name;
+    private String content;
+    private LocalTime time;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+}
