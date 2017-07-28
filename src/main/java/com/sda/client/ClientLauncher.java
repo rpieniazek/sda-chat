c2bba68ca2de52641b4f6de0dca9b75eaebc99ec
RENTdanplo <RENT@RENT-Komputer>
2017-07-28 18:58:36 +0200

    System properties file loading

--- src/main/java/com/sda/client/ClientLauncher.java
+++ src/main/java/com/sda/client/ClientLauncher.java
@@ -1,5 +1,7 @@
 package com.sda.client;
 
+import com.sda.commons.ConfigService;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Properties;
@@ -10,15 +12,7 @@ import java.util.Properties;
 public class ClientLauncher {
 
     public static void main(String[] args) {
-        try {
-            FileInputStream fileInputStream = new FileInputStream("application.properties");
-            Properties properties = new Properties(System.getProperties());
-            properties.load(fileInputStream);
-            System.setProperties(properties);
-            System.out.println(System.getProperty("server.ip"));
-        } catch (java.io.IOException e) {
-            e.printStackTrace();
-        }
+        ConfigService.loadProperties();
         new ClientController();
     }
 }
---
+++ src/main/java/com/sda/commons/ConfigService.java
@@ -0,0 +1,21 @@
+package com.sda.commons;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Created by RENT on 2017-07-28.
+ */
+public class ConfigService {
+    public static void loadProperties(){
+        try {
+            FileInputStream fileInputStream = new FileInputStream("application.properties");
+            Properties properties = new Properties(System.getProperties());
+            properties.load(fileInputStream);
+            System.setProperties(properties);
+            System.out.println(System.getProperty("server.ip"));
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
