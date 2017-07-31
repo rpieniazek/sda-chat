99ee8771f39fc56285db0ac3de6ab445132939da
RENTdanplo <RENT@RENT-Komputer>
2017-07-31 18:09:01 +0200

    server side init for handling username - messageDto, name ranamed to senderName

--- src/main/java/com/sda/commons/MessageDto.java
+++ src/main/java/com/sda/commons/MessageDto.java
@@ -2,12 +2,9 @@ package com.sda.commons;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalTimeSerializer;
 
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 import static java.lang.String.*;
@@ -16,7 +13,7 @@ import static java.lang.String.*;
  * Created by RENT on 2017-07-27.
  */
 public class MessageDto {
-    private String name;
+    private String senderName;
 
     private String content;
 
@@ -29,16 +26,16 @@ public class MessageDto {
 
     public MessageDto(String content) {
         this.content = content;
-        this.name = "Rafal";
+        this.senderName = "Rafal";
         this.time = LocalTime.now();
     }
 
-    public String getName() {
-        return name;
+    public String getSenderName() {
+        return senderName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
     }
 
     public String getContent() {
@@ -59,6 +56,6 @@ public class MessageDto {
 
     @Override
     public String toString() {
-        return format("<%s|%s> %s\n", time.toString().substring(0, 8), name, content);
+        return format("<%s|%s> %s\n", time.toString().substring(0, 8), senderName, content);
     }
 }
