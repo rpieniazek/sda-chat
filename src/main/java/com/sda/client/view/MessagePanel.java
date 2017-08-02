9e980121aa54605032a7dbcc224ac0a4583b2b22
RENTdanplo <RENT@RENT-Komputer>
2017-08-02 20:30:31 +0200

    messagePanel refactoring cont.

--- src/main/java/com/sda/client/view/MessagePanel.java
+++ src/main/java/com/sda/client/view/MessagePanel.java
@@ -10,8 +10,8 @@ public class MessagePanel extends JPanel {
     private ClientView clientView;
 
     private JButton sendMessageButton;
-    private JTextField messageInputField; //wpisywana wiadomosc
-    private JTextArea messagesList; //lista wiadomosci
+    private JTextField messageInputField;
+    private JTextArea messagesList;
 
     public MessagePanel(ClientView clientView) {
         this.clientView = clientView;
@@ -24,24 +24,9 @@ public class MessagePanel extends JPanel {
 
     private void createMessagePanel() {
         setLayout(new BorderLayout());
-        addMessageInput();
-        addSendMessageButton();
+        setSize(400, 200);
         addMessageList();
         addMessageLinePanel();
-        setSize(400, 200);
-    }
-
-    private void addMessageInput() {
-        messageInputField = new JTextField(30);
-        messageInputField.requestFocusInWindow();
-    }
-
-    private void addSendMessageButton() {
-        sendMessageButton = new JButton("Send Message");
-        sendMessageButton.addActionListener(event -> {
-            sendMessage();
-        });
-        clientView.setDefaultButton(sendMessageButton);
     }
 
     private void addMessageList() {
@@ -54,27 +39,46 @@ public class MessagePanel extends JPanel {
     }
 
     private void addMessageLinePanel() {
-        GridBagConstraints left = new GridBagConstraints();
-        left.anchor = GridBagConstraints.LINE_START;
-        left.fill = GridBagConstraints.HORIZONTAL;
-        left.weightx = 512.0D;
-        left.weighty = 1.0D;
+        JPanel messageLinePanel = new JPanel();
+        createMessageInput();
+        createSendMessageButton();
+        messageLinePanel.setBackground(Color.BLUE);
+        messageLinePanel.setLayout(new GridBagLayout());
+        messageLinePanel.add(messageInputField, createLeftConstraint());
+        messageLinePanel.add(sendMessageButton, createRightConstraint());
+        add(BorderLayout.SOUTH, messageLinePanel);
+    }
 
+    private void createMessageInput() {
+        messageInputField = new JTextField(30);
+        messageInputField.requestFocusInWindow();
+    }
+
+    private void createSendMessageButton() {
+        sendMessageButton = new JButton("Send Message");
+        sendMessageButton.addActionListener(event -> {
+            sendMessage();
+        });
+        clientView.setDefaultButton(sendMessageButton);
+    }
+
+    private GridBagConstraints createRightConstraint() {
         GridBagConstraints right = new GridBagConstraints();
         right.insets = new Insets(0, 10, 0, 0);
         right.anchor = GridBagConstraints.LINE_END;
         right.fill = GridBagConstraints.NONE;
         right.weightx = 1.0D;
         right.weighty = 1.0D;
+        return right;
+    }
 
-        JPanel messageLinePanel = new JPanel();
-        messageLinePanel.setBackground(Color.BLUE);
-        messageLinePanel.setLayout(new GridBagLayout());
-
-        messageLinePanel.add(messageInputField, left);
-        messageLinePanel.add(sendMessageButton, right);
-
-        add(BorderLayout.SOUTH, messageLinePanel);
+    private GridBagConstraints createLeftConstraint() {
+        GridBagConstraints left = new GridBagConstraints();
+        left.anchor = GridBagConstraints.LINE_START;
+        left.fill = GridBagConstraints.HORIZONTAL;
+        left.weightx = 512.0D;
+        left.weighty = 1.0D;
+        return left;
     }
 
     private void sendMessage() {
